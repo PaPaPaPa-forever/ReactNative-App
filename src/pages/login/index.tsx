897010cbeaa5c4157e26a92a6d3311acc7a98d52
LUAN SALVES FERREIRA SANTOS <luan.8966@aluno.pr.senac.br>
2025-08-18 15:12:54 -0300

    first commit

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { style } from "./style";
+
+export default function login() {
+    return (
+        < View style={style.container}>
+            <View style={style.boxTop}>
+                <Text>Top</Text>
+            </View>
+            <View style={style.boxMid}>
+                <Text>Mid</Text>
+            </View>
+            <View style = {style.boxBotton}>
+                <Text>Botton</Text>
+            </View>
+        </View>
+    )
+}
