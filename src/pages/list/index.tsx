8a885bb3a85a55e5198d72652b0f678548632154
LUAN SALVES FERREIRA SANTOS <luan.8966@aluno.pr.senac.br>
2025-09-22 10:04:41 -0300

    Criando FlatList para renderização, criando também tarefas em array para serem exibidas com FlatList

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,10 +1,58 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { FlatList } from "react-native";
+import { style } from "./styles";
+import { Input } from "../../components/input";
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function List () {
+type PropCard = {
+    item: number,
+    title: string,
+    description: string,
+    flag: 'urgente' | 'opcional'
+}
+
+const data: Array<PropCard> = [
+    {
+        item: 0,
+        title: 'Realizar Lição de casa',
+        description: 'Página 18 ao 28',
+        flag: 'urgente'
+    },
+    {
+        item: 1,
+        title: 'Passear com o cachorro',
+        description: 'Página 18 ao 28',
+        flag: 'urgente'
+    },
+    {
+        item: 2,
+        title: 'Sair para comer sortão',
+        description: 'Página 18 ao 28',
+        flag: 'urgente'
+    },
+]
+export default function List() {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Lista</Text>
+        <View style={style.container}>
+            <View style={style.header}>
+                <Text style={style.greeting}>Bom dia,
+                    <Text style={{ fontWeight: 'bold' }}> Leonardo Pires</Text></Text>
+                <View style={style.boxInput}>
+                    <Input
+                        IconLeft={MaterialIcons}
+                        IconLeftName="search"
+                    />
+                </View>
+            </View>
+            <View style={style.boxList}>
+                <FlatList 
+                    data={data}
+                    style={{marginTop: 40, paddingHorizontal: 30}}
+                    keyExtractor={(item, index) => item.item.toString()}
+                    renderItem={({item, index}) => {return(<Text>{item.title}</Text>)}}
+                />
+            </View>
         </View>
     )
 }
